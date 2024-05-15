9e58629dc90abe774e46a2a6105a775aee7e6961
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-15 14:30:27 +0300

    fix: rubocop errors in `app/controllers/concerns/stkpush.rb`

--- app/controllers/concerns/mpesas/stkpush.rb
+++ app/controllers/concerns/mpesas/stkpush.rb
@@ -1,50 +1,62 @@
 # frozen_string_literal: true
 
-# Merchant initiated request (C2B), send a payment prompt on the customer's phone (Popularly known as STK Push Prompt)
+module Mpesas
+  # Merchant initiated request (C2B), send a payment prompt on the customer's phone (Popularly known as STK Push Prompt)
+  class Stkpush
+    extend ActiveSupport::Concern
 
-module Mpesas::Stkpush
-  extend ActiveSupport::Concern
+    def stkpush
+      phone_number = params[:phone_number]
+      amount = params[:amount]
 
-  def stkpush
-    phoneNumber = params[:phoneNumber]
-    amount = params[:amount]
-    payload = {
-    'BusinessShortCode': @short_code,
-    'Password': @password,
-    'Timestamp': @timestamp,
-    'TransactionType': "CustomerPayBillOnline",
-    'Amount': amount,
-    'PartyA': phoneNumber,
-    'PartyB': @short_code,
-    'PhoneNumber': phoneNumber,
-    'CallBackURL': "#{@callback_url}/callback_url",
-    'AccountReference': 'Codearn',
-    'TransactionDesc': "Payment for Codearn premium"
-    }.to_json
+      payload = generate_payload(phone_number, amount)
+      headers = generate_headers
 
-    headers = {
-    Content_type: 'application/json',
-    Authorization: "Bearer #{access_token}"
-    }
-
-    response = RestClient::Request.new({
-    method: :post,
-    url: @stkpush_url,
-    payload: payload,
-    headers: headers
-    }).execute do |response, request|
-    case response.code
-    when 500
-    [ :error, JSON.parse(response.to_str) ]
-    when 400
-    [ :error, JSON.parse(response.to_str) ]
-    when 200
-    [ :success, JSON.parse(response.to_str) ]
-    else
-    fail "Invalid response #{response.to_str} received."
+      response = send_request(payload, headers)
+      render json: response
     end
+
+    private
+
+    def generate_payload(phone_number, amount)
+      {
+        'BusinessShortCode': @short_code,    'Password': @password,
+        'Timestamp': @timestamp,             'TransactionType': 'CustomerPayBillOnline',
+        'Amount': amount,                    'PartyA': phone_number,
+        'PartyB': @short_code,               'PhoneNumber': phone_number,
+        'CallBackURL': "#{@callback_url}/callback_url",
+        'AccountReference': 'Ruby Daraja',
+        'TransactionDesc': 'Ruby Daraja Api Stk push test'
+      }.to_json
     end
-    render json: response
+
+    def generate_headers
+      {
+        Content_type: 'application/json',
+        Authorization: "Basic #{access_token}"
+      }
+    end
+
+    def send_request(payload, headers)
+      RestClient::Request.new(
+        method: :post,
+        url: @stkpush_url,
+        payload:,
+        headers:
+      ).execute do |response, _request|
+        handle_response(response)
+      end
+    end
+
+    def handle_response(response)
+      case response.code
+      when 200
+        [:success, JSON.parse(response.to_str)]
+      when 400, 500
+        [:error, JSON.parse(response.to_str)]
+      else
+        raise "Invalid response #{response.to_str} received."
+      end
+    end
   end
-
 end
