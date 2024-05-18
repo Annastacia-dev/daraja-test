685323cabad13a9343f7d12205a000a4d51cb888
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-18 22:53:52 +0300

    express guide: update load variables methos + define concern + add interface showig the stk prompt

--- app/controllers/concerns/mpesa/stkpush.rb
+++ app/controllers/concerns/mpesa/stkpush.rb
@@ -9,7 +9,7 @@ module Mpesa
 
       respond_to do |format|
         if phone_number.blank? || amount.blank?
-          format.html { redirect_to mpesa_express_path, alert: 'Phone number or amount is blank' }
+          format.html { redirect_to mpesa_express_path, success: 'Phone number or amount is blank' }
           format.json { render json: { message: 'Error: Phone number or amount is blank '} }
         else
           regex = /\A2547\d{8}\z/
@@ -17,7 +17,7 @@ module Mpesa
             payload = generate_stkpush_payload(phone_number, amount)
             headers = generate_headers
             response = send_stkpush_request(payload, headers)
-            format.html { redirect_to mpesa_express_path, notice: 'Request in progress'}
+            format.html { redirect_to mpesa_express_path, notice: "#{ response[1]["CustomerMessage"]}"}
             format.json { render json: response }
           else
             format.html { redirect_to mpesa_express_path, alert: 'Phone number format is invalid should be in the format 2547xxxxxxx' }
